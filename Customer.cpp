5173997929c92b800e95e9597a00821954ac1ab1
kurisukun <barroshenriques.chris@gmail.com>
2020-06-18 10:45:00 +0200

    Replace for-loop with for-each

--- Customer.cpp
+++ Customer.cpp
@@ -12,38 +12,39 @@ string Customer::statement()
 {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
-    vector< Rental >::iterator iter = _rentals.begin();
+
     vector< Rental >::iterator iter_end = _rentals.end();
     ostringstream result;
     result << "Rental Record for " << getName() << "\n";
-    for ( ; iter != iter_end; ++iter ) {
+
+    for(const auto& rental: _rentals){
         double thisAmount = 0;
 
         // determine amounts for each line
-        switch ( (*iter).getMovie().getPriceCode() ) {
+        switch (rental.getMovie().getPriceCode()){
             case Movie::REGULAR:
                 thisAmount += 2;
-                if ((*iter).getDaysRented() > 2 )
-                    thisAmount += ((*iter).getDaysRented() - 2 ) * 1.5 ;
+                if (rental.getDaysRented() > 2 )
+                    thisAmount += (rental.getDaysRented() - 2 ) * 1.5 ;
                 break;
             case Movie::NEW_RELEASE:
-                thisAmount += (*iter).getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie::CHILDRENS:
                 thisAmount += 1.5;
-                if ((*iter).getDaysRented() > 3 )
-                    thisAmount += ((*iter).getDaysRented() - 3 ) * 1.5;
+                if (rental.getDaysRented() > 3 )
+                    thisAmount += (rental.getDaysRented() - 3 ) * 1.5;
                 break;
         }
 
         // add frequent renter points
         frequentRenterPoints++;
         // add bonus for a two day new release rental
-        if (((*iter).getMovie().getPriceCode() == Movie::NEW_RELEASE )
-            && (*iter).getDaysRented() > 1 ) frequentRenterPoints++;
+        if ((rental.getMovie().getPriceCode() == Movie::NEW_RELEASE )
+            && rental.getDaysRented() > 1 ) frequentRenterPoints++;
 
         // show figures for this rental
-        result << "\t" << (*iter).getMovie().getTitle() << "\t"
+        result << "\t" << rental.getMovie().getTitle() << "\t"
                << thisAmount << "\n";
         totalAmount += thisAmount;
     }
