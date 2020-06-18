2446e868e8240296c1b2ff96f669873f4910ea95
Doran Kayoumi <dorankayoumi@gmail.com>
2020-06-18 13:18:19 +0200

    Add unit tests for getFrequentRenterPoints

--- tests/MovieTest.cpp
+++ tests/MovieTest.cpp
@@ -27,3 +27,28 @@ TEST(NewReleaseMovie, getPrice){
     ASSERT_EQ(movie.getPrice(2), 6);
     ASSERT_EQ(movie.getPrice(3), 9);
 }
+
+
+TEST(RegularMovie, getFrequentRenterPoints){
+    Movie movie = Movie("Regular Movie", new MovieStateRegular());
+
+    ASSERT_EQ(movie.getFrequentRenterPoints(1), 1);
+    ASSERT_EQ(movie.getFrequentRenterPoints(2), 1);
+    ASSERT_EQ(movie.getFrequentRenterPoints(3), 1);
+}
+
+TEST(ChildrenMovie, getFrequentRenterPoints){
+    Movie movie = Movie("Children Movie", new MovieStateChildren());
+
+    ASSERT_EQ(movie.getFrequentRenterPoints(1), 1);
+    ASSERT_EQ(movie.getFrequentRenterPoints(2), 1);
+    ASSERT_EQ(movie.getFrequentRenterPoints(4), 1);
+}
+
+TEST(NewReleaseMovie, getFrequentRenterPoints){
+    Movie movie = Movie("New Release Movie", new MovieStateNewRelease());
+
+    ASSERT_EQ(movie.getFrequentRenterPoints(1), 1);
+    ASSERT_EQ(movie.getFrequentRenterPoints(2), 2);
+    ASSERT_EQ(movie.getFrequentRenterPoints(3), 2);
+}
