427eb589fe15bc7ddf2a9edbe87d876516dae9e8
Doran Kayoumi <dorankayoumi@gmail.com>
2020-06-18 12:46:43 +0200

    Add unit test for Regular movie state renter points

--- tests/MovieStatesTest.cpp
+++ tests/MovieStatesTest.cpp
@@ -11,3 +11,11 @@ TEST(MovieStateRegular, getPrice){
     ASSERT_EQ(movieStateRegular.getPrice(2), 2);
     ASSERT_EQ(movieStateRegular.getPrice(3), 3.5);
 }
+
+TEST(MovieStateRegular, getRenterPoints){
+
+    MovieStateRegular movieStateRegular;
+
+    ASSERT_EQ(movieStateRegular.getRenterPoints(1), 1);
+    ASSERT_EQ(movieStateRegular.getRenterPoints(2), 1);
+}
