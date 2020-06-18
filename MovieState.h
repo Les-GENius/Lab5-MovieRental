10cda38a58aa7a8b3fb46593944681272da64d0e
kurisukun <barroshenriques.chris@gmail.com>
2020-06-18 11:02:05 +0200

    MovieState class created

---
+++ MovieState.h
@@ -0,0 +1,17 @@
+//
+// Created by kurisukun on 18.06.20.
+//
+
+#ifndef LAB05_MOVIESTATE_H
+#define LAB05_MOVIESTATE_H
+
+
+class MovieState {
+
+    virtual double getPrice(int nbDays) = 0;
+    virtual int getRenterPoints(int nbDays) = 0;
+    
+};
+
+
+#endif //LAB05_MOVIESTATE_H
