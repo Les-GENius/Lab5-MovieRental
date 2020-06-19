77b7921afe6e4252bbe5794982e60fcdce462fad
Doran Kayoumi <dorankayoumi@gmail.com>
2020-06-19 16:44:58 +0200

    Add unit test for getFrequentRenterPoints

--- tests/CustomerTest.cpp
+++ tests/CustomerTest.cpp
@@ -1,5 +1,9 @@
 #include <Customer.h>
 #include <gtest/gtest.h>
+
+#include <tests/mock/RentalMock.h>
+#include <tests/mock/MovieMock.h>
+
 #include <MovieStateRegular.h>
 #include <MovieStateChildren.h>
 #include <MovieStateNewRelease.h>
@@ -19,5 +23,21 @@ TEST(Customer, statement){
                     "You earned 4 frequent renter points";
 
     ASSERT_EQ(customer.statement(), s);
-
 };
+
+
+TEST(Customer, GetFrequentRenterPoints) {
+    MockMovieStateNewRelease state;
+    EXPECT_CALL(state, getBonusRenterPoints(2)).WillOnce(testing::Return(1));
+
+    MockMovie movie("Super Mocked Movie", &state);
+    EXPECT_CALL(movie, getFrequentRenterPoints(2)).WillRepeatedly(testing::Return(0));
+
+    MockRental mock(movie, 2);
+    EXPECT_CALL(mock, getRenterPoints()).WillRepeatedly(testing::Return(1));
+
+    Customer customer("Bob");
+    customer.addRental(mock);
+
+    ASSERT_EQ(customer.getFrequentRenterPoints(), 2);
+}
